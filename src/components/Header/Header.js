3ae88fb301168b1a91f5f8c18f763cbc87769881
Rocko <codingwithrock@gmail.com>
2022-09-26 18:18:30 +0600

    header section are ready

---
+++ src/components/Header/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Header.css';
+import logo from '../../images/Logo.svg';
+
+
+const Header = () => {
+    return (
+        <nav>
+            {/* LOGO */}
+            <div>
+                <img src={logo} alt="" />
+            </div>
+
+
+            {/* Nav Menu Section Start*/}
+            <div className='nav-menu-link'>
+                <a>Order</a>
+                <a>Order Review</a>
+                <a>Manage Review</a>
+                <a>Login</a>
+            </div>
+            {/* Nav Menu Section End*/}
+
+        </nav>
+    );
+};
+
+export default Header;
